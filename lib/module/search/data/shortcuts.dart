5ab60a1fbb64c6eb5fdec2aa86ec61e29e4364d5
Muhammad firdaus Ramadhan <adam.id9999@gmail.com>
2024-05-18 19:19:24 +0800

    first init github

---
+++ lib/module/search/data/shortcuts.dart
@@ -0,0 +1,27 @@
+List<Map<String, String>> shortcuts = [
+  {
+    "address": "Jl. Juanda ",
+    "detail_address": "Jl. Juanda No.54, Samarinda City",
+    "distance": "0.5 km"
+  },
+  {
+    "address": "Jl. M.Yamin",
+    "detail_address": "Jl. M.Yamin No.20, Samarinda City",
+    "distance": "0.7 km"
+  },
+  {
+    "address": "Jl. Pangeran Antasari",
+    "detail_address": "Jl. Pangeran Antasari City No.54, Samarinda City",
+    "distance": "0.3 km"
+  },
+  {
+    "address": "Jl. Abdul Wahab Syahranie",
+    "detail_address": "Jl. Abdul Wahab Syahranie No.54, Samarinda City",
+    "distance": "3.5 km"
+  },
+  {
+    "address": "Jl. Loa Bakung",
+    "detail_address": "Jl. Loa Bakung No.54, Samarinda City",
+    "distance": "1.7 km"
+  }
+];
